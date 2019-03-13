1b9bd343d1907d981ebcea6f4e6e2caf181ae240
abberispe <43879874+abberispe@users.noreply.github.com>
2019-03-13 15:25:26 +0100

    Update productsjs.js

--- productsjs.js
+++ productsjs.js
@@ -1,28 +1,4 @@
-var isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-    
-    if (isMobile.any()) {
-        $("#entireSite").empty();
-        $("#entireSite").append("<h3 style='margin-top:100px' text-align='center'>I nuläget är hemsidan inte kompatibel med mobiltelefoner. Vänligen handla via din dator istället.</h3>");
-    }
+
         const url = "https://gg71nquqt8.execute-api.us-east-1.amazonaws.com/prod/productsLambdaReal?type=GET"; // site that doesn’t send Access-Control-*
         fetch(url) // https://cors-anywhere.herokuapp.com/https://example.com
             .then(response => response.text())
